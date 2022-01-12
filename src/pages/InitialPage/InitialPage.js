aec557d02dbea4d5d63752ffae9507e790db4aec
soraia-aparecida <soraia211norte@gmail.com>
2022-01-12 20:34:20 -0300

    ir para home, ok. E cor dos icones do footer, ok

--- src/pages/InitialPage/InitialPage.js
+++ src/pages/InitialPage/InitialPage.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { ContainerInitialPage } from './styled';
 import { CardMedia } from '@material-ui/core';
 import LogoImg from '../../assests/logo.png'
+import { useNavigate } from 'react-router-dom';
+import { goToHome } from '../../router/coordinator';
 
 const InitialPage = () => {
+  const history = useNavigate()
   return (
     <ContainerInitialPage>
       <CardMedia
         component="img"
         image={LogoImg}
         alt="pokemons"
+        onClick={() => goToHome(history)}
       />
 
     </ContainerInitialPage>
