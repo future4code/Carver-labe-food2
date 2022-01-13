d09f6bafdf69a163e41a24acde1822f9aa54a578
yanikisia <kathlyn.yary@gmail.com>
2022-01-13 16:54:59 -0300

    requisiçao da home e buscar

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -12,6 +12,7 @@ import styled from "styled-components";
 import { Text, TabsStyled, UnderTextCard,  ContainerCardUnderText, SeachContainer, DivStyled } from "./styled";
 import GlobalStateContext from "../../contexts/GlobalStateContext";
 import { goToRestaurantDetails } from "../../router/coordinator";
+import useRequestData from "../../hooks/useRequestData";
 
 const useStyles1 = makeStyles((theme) => ({
     menuButton: {
@@ -111,82 +112,12 @@ const SeachPage = () => {
     const [value, setValue] = React.useState("");
     const [loading, setLoading] = React.useState(true)
     const {states,setters,requests}= useContext(GlobalStateContext)
-    const restaurant=[ {
-        "id": "1",
-        "category": "Árabe",
-        "description": "Habib's é uma rede de restaurantes de comida rápida brasileira especializada em culinária árabe, os restaurantes vendem mais de 600 milhões de esfirras por ano. A empresa emprega 22 mil colaboradores e tem 421 unidades distribuídas em mais de cem municípios em 20 unidades federativas.",
-        "address": "Rua das Margaridas, 110 - Jardim das Flores",
-        "logoUrl": "https://firebasestorage.googleapis.com/v0/b/missao-newton.appspot.com/o/futureFoodsRestaurants%2Fhabibs.jpg?alt=media&token=a30ea547-3a3b-4e80-b58e-b8dc976697de",
-        "name": "Habibs",
-        "shipping": 6,
-        "deliveryTime": 60
-    },
-    {
-        "id": "10",
-        "description": "Restaurante sofisticado busca o equilíbrio entre ingredientes que realçam a experiência da culinária japonesa.",
-        "logoUrl": "https://firebasestorage.googleapis.com/v0/b/missao-newton.appspot.com/o/futureFoodsRestaurants%2Ftadashii.png?alt=media&token=0e4b9b8c-3b6e-4a78-bd49-d3576657a342",
-        "deliveryTime": 50,
-        "category": "Asiática",
-        "shipping": 13,
-        "address": "Travessa Reginaldo Pereira, 130 - Ibitinga",
-        "name": "Tadashii"
-    },
-    {
-        "id": "2",
-        "address": "Avenida dos Papagaios, 1350 - Sta. Efigênia",
-        "description": "McDonald's Corporation é a maior cadeia mundial de restaurantes de fast food de hambúrguer, servindo cerca de 68 milhões de clientes por dia em 119 países através de 37 mil pontos de venda.",
-        "shipping": 19,
-        "deliveryTime": 15,
-        "logoUrl": "https://firebasestorage.googleapis.com/v0/b/missao-newton.appspot.com/o/futureFoodsRestaurants%2Fmcdonalds.png?alt=media&token=87a0fc52-3b8d-4475-b3e4-6ff730356121",
-        "category": "Hamburguer",
-        "name": "McDonalds"
-    },
-    {
-        "id": "3",
-        "logoUrl": "https://firebasestorage.googleapis.com/v0/b/missao-newton.appspot.com/o/futureFoodsRestaurants%2Fcantinamammaperrotta.jpg?alt=media&token=3a4e76b6-3d07-414b-8672-e624f5a10a76",
-        "category": "Italiana",
-        "address": "Rua Barão do Rio Branco, 98 - Centro",
-        "deliveryTime": 20,
-        "description": "Restaurante Self Service e lanchonete localizado no Laboratório Nacional de Computação Científica",
-        "shipping": 2,
-        "name": "Cantina Mamma Perrotta"
-    },
-    {
-        "id": "4",
-        "address": "Travessa Junqueira de Melo, 315 - Marginal",
-        "name": "Sorveteria Bacio di Latte",
-        "shipping": 10,
-        "deliveryTime": 45,
-        "description": "Gelatos de raízes italianas feitos artesanalmente e com ingredientes de altíssima qualidade. Confira todos os nossos deliciosos sabores!",
-        "category": "Sorvetes",
-        "logoUrl": "https://firebasestorage.googleapis.com/v0/b/missao-newton.appspot.com/o/futureFoodsRestaurants%2Fbaciodilatte.png?alt=media&token=03839175-d9b3-4024-a516-5ce2cbfc6f30"
-    },
-    {
-        "id": "5",
-        "shipping": 18,
-        "category": "Carnes",
-        "description": "Inaugurado em 1988 nos Estados Unidos e chegou ao Brasil 9 anos depois. Hoje, o restaurante marca presença em 20 cidades em todo o país, com um estilo casual que vai fazer você se sentir no Outback Australiano",
-        "name": "Outback Steakhouse",
-        "address": "Alameda dos Marsupiais, 505 - Humaitá",
-        "deliveryTime": 20,
-        "logoUrl": "https://firebasestorage.googleapis.com/v0/b/missao-newton.appspot.com/o/futureFoodsRestaurants%2Foutback.png?alt=media&token=6c3af525-e6f5-4f96-b5d5-a0ad6702a838"
-    },
-    {
-        "id": "6",
-        "logoUrl": "https://firebasestorage.googleapis.com/v0/b/missao-newton.appspot.com/o/futureFoodsRestaurants%2Fsotero.jpg?alt=media&token=c8760cea-f8fc-4f66-b4b3-5c937beb8fe2",
-        "category": "Baiana",
-        "name": "Sotero Cozinha Original",
-        "description": "Culinária baiana, como caldinho de sururu e acarajé, empório nordestino e bar com 400 rótulos de cachaça.",
-        "shipping": 4,
-        "deliveryTime": 40,
-        "address": "Rua Dorival Caymmi, 149 - Alto dos Ibirás"
-    }]
-
+    const restaurant=useRequestData('https://us-central1-missao-newton.cloudfunctions.net/futureEatsA/restaurants',[])
+    
 
     const handleChange = (event, newValue) => {
       
         setValue(event.target.value)
-        console.log(event.target.value.length)
         if(event.target.value.length==0  ){
             setLoading(true)
         }
@@ -196,12 +127,12 @@ const SeachPage = () => {
     };
 
 
-    const cardFilter= restaurant.filter((restaurant) => {
+    const cardFilter= restaurant.restaurants&&restaurant.restaurants.filter((restaurant) => {
         return restaurant.name.toLowerCase().includes(value.toLowerCase()) 
       })
  
 
-     const card=cardFilter.map((restaurant)=>{
+     const card=restaurant.restaurants&&cardFilter.map((restaurant)=>{
         
          return ( 
          <Card key={restaurant.id}  onClick={()=> goToRestaurantDetails(navigate,restaurant.id)} className={classes2.root}>
