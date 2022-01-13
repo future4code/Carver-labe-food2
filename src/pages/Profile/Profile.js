121695ddb7a8fedc1b1c48b3b95ae30dde12b545
soraia-aparecida <soraia211norte@gmail.com>
2022-01-13 17:45:58 -0300

    função de logout

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,22 +1,30 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { AddressInformation, ButtonContainer, GrayText, Line, OrderDate, OrderHistoryArea, OrderHistoryCard, OrderHistoryTitleCard, OrderTotalCost, PersonalInformationProfileArea, ProfilePageContainer } from './profile.css.js'
+import { AddressInformation, ButtonContainer, GrayText, Line, OrderDate, OrderHistoryArea, OrderHistoryCard, OrderHistoryTitleCard, OrderTotalCost, PersonalInformationProfileArea, ProfilePageContainer, BotaoLogout } from './profile.css.js'
 import IconButton from '@material-ui/core/IconButton';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import CreateOutlinedIcon from '@material-ui/icons/CreateOutlined';
 import GlobalStateContext from '../../contexts/GlobalStateContext.js';
 import { getFullAddress } from '../../services/services.js';
 import { useNavigate } from 'react-router-dom';
+import { goToLogin } from '../../router/coordinator.js';
+import { Button } from '@material-ui/core';
 
 export default function Profile() {
-    const {states, setters, requests} = useContext(GlobalStateContext)
+    const { states, setters, requests } = useContext(GlobalStateContext)
     const [address, setAddress] = useState()
     const [loading, setLoading] = useState(true)
     const navigate = useNavigate()
+    const token = localStorage.getItem('token')
+
+    const clear = () => {
+        localStorage.removeItem('token')
+        goToLogin(navigate)
+    }
 
     useEffect(() => {
-        getFullAddress().then(res=>{
+        getFullAddress().then(res => {
             setAddress(res.data.address)
-            setters.setUser({...states.user, address: res.data.address})
+            setters.setUser({ ...states.user, address: res.data.address })
             setLoading(false)
         })
     }, [])
@@ -28,35 +36,35 @@ export default function Profile() {
                 <h5>{states.user.email}</h5>
                 <h5>{states.user.cpf}</h5>
 
-                <ButtonContainer onClick={()=> navigate('/cadastro')}>
+                <ButtonContainer onClick={() => navigate('/cadastro')}>
                     <IconButton aria-label="delete">
                         <CreateOutlinedIcon />
                     </IconButton>
                 </ButtonContainer>
-                
+
             </PersonalInformationProfileArea>
 
             {
                 loading
-                ?<>
-                    <CircularProgress/>
-                    <h2>Carregando...</h2>
-                </>
-                :<AddressInformation>
-                    <GrayText>Endereço cadastrado</GrayText>
-                    <h5>{`${address.neighbourhood}, ${address.number} - ${address.city}`}</h5>
-                    <ButtonContainer onClick={() => navigate('/cadastrar-endereco')}>
-                        <IconButton aria-label="delete">
-                            <CreateOutlinedIcon />
-                        </IconButton>
-                    </ButtonContainer>
-                </AddressInformation>
+                    ? <>
+                        <CircularProgress />
+                        <h2>Carregando...</h2>
+                    </>
+                    : <AddressInformation>
+                        <GrayText>Endereço cadastrado</GrayText>
+                        <h5>{`${address.neighbourhood}, ${address.number} - ${address.city}`}</h5>
+                        <ButtonContainer onClick={() => navigate('/cadastrar-endereco')}>
+                            <IconButton aria-label="delete">
+                                <CreateOutlinedIcon />
+                            </IconButton>
+                        </ButtonContainer>
+                    </AddressInformation>
 
             }
 
             <OrderHistoryArea>
                 <h5>Histórico de pedidos</h5>
-                <Line/>
+                <Line />
                 <OrderHistoryCard>
                     <OrderHistoryTitleCard>Bullguer Vila Madalena</OrderHistoryTitleCard>
                     <OrderDate>23 outubro 2019</OrderDate>
@@ -77,7 +85,14 @@ export default function Profile() {
                     <OrderDate>23 outubro 2019</OrderDate>
                     <OrderTotalCost>Total R$67,00</OrderTotalCost>
                 </OrderHistoryCard>
+
+                < BotaoLogout>
+                <Button variant="contained" color="primary" onClick={clear}>
+                    Logout
+                </Button>
+            </ BotaoLogout>
             </OrderHistoryArea>
+            
         </ProfilePageContainer>
     )
 }
